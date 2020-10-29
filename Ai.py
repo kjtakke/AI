86319b546bddacd57835293fbf209e53fd12718b
Kristopher Takken <63102467+kjtakke@users.noreply.github.com>
2020-10-29 22:14:14 +1030

    Update Ai.py

--- Ai.py
+++ Ai.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 import os
+import pprint
 
 class AI_Phraser:
     """This class enables the creation an AI infrastructure"""
@@ -12,78 +13,234 @@ class AI_Phraser:
         self.reiterateMatchedItems = reiterateMatchedItems
         self.reiterateAppendedItems = reiterateAppendedItems
 
-    def _breakdown(self):
+
+    def _breakdown(self, itemName):
         """This function develops the JSON format for items to be compared"""
-        pass
+        if self.appendUnmatchedItems == True:
+            append = "transient"
+        else:
+            append = "fixed"
+        compareDictionary = {}
+        compare = compareDictionary[itemName] = {
+            "type": append,
+            "passed":[],
+            "two": {
+                "first":[],
+                "mid":[],
+                "end":[]
+            },
+            "three": {
+                "first":[],
+                "mid":[],
+                "end":[]
+            },
+            "four": {
+                "first":[],
+                "mid":[],
+                "end":[]
+            },
+            "scores":{
+                "allScores": [],
+                "statistics": {
+                    "min": 0,
+                    "max": 0,
+                    "median": 0,
+                    "average": 0,
+                    "stDev": 0,
+                    "sum": 0
+                },
+            },
+            "values": []
+        }
+
+        return compare
+
+    def _deliminatBreakdown(self, itemName, fixed):
+        """This function develops the JSON format for items to be compared"""
+        if fixed == True:
+            append = "transient"
+        else:
+            append = "fixed"
+        compareDictionary = {}
+        compare = compareDictionary[itemName] = {
+            "type": append,
+            "passed":[],
+            "two": {
+                "first":[],
+                "mid":[],
+                "end":[]
+            },
+            "three": {
+                "first":[],
+                "mid":[],
+                "end":[]
+            },
+            "four": {
+                "first":[],
+                "mid":[],
+                "end":[]
+            },
+            "scores":{
+                "allScores": [],
+                "statistics": {
+                    "min": 0,
+                    "max": 0,
+                    "median": 0,
+                    "average": 0,
+                    "stDev": 0,
+                    "sum": 0
+                },
+            },
+            "values": []
+        }
+
+        return compare
+
+    ##################################################################################################################################
+    # NEED TO ADD PHRASE BREAKDOWN Two, Three, Four
+    ##################################################################################################################################
+    def load(self, file, indexColumn, newFileName, printLastItem=False):
+        df = pd.read_csv(file, header=0, dtype=str)
+        index = df[indexColumn].tolist()
+        compareDictionary = {}
+        for item in index:
+            try:    #Duplicate items are not loaded
+                compareDictionary[item] = (self._breakdown(item))
+            except:
+                pass
+
+        try:    #Create JSON file
+            with open(newFileName + ".json", 'w') as fp:
+                json.dump(compareDictionary, fp)
+        except:
+            print("Invalid JSON file")
+
+        if printLastItem == True:
+            try:
+                pp = pprint.PrettyPrinter(indent=2, width=80)
+                pp.pprint(compareDictionary[item])
+                compareDictionary[item] = (self._breakdown(item))
+            except:
+                print("Invalid last item")
+
+
+    def _blank_canvas(self, fileName):
+        """This function creates a blank JSON canvas which is used if no reference file is identified/loaded"""
+        try:    #Create JSON file
+            indexDictionary = {}
+            with open(fileName + ".json", 'w') as fp:
+                json.dump(indexDictionary, fp)
+        except:
+            print("Error creating loading file")
+
 
     def _scoring(self):
         """This function creates the neural mapping by adding or subtracting scores"""
         pass
 
-    def _blank_canvas(self):
-        """This function creates a blank JSON canvas which is used if no reference file is identified/loaded"""
-        pass
 
     def _compare(self):
         """This function compares two phrases before scoring"""
         pass
 
+
     def _init_iterate(self):
         """This function iterates through a given list"""
         pass
+
 
     def _iterate_matched_items(self):
         """This function iterates through all matched items"""
         pass
 
+
     def _iterate_transient_items(self):
         """This function iterates through all items labled 'transient'"""
         pass
+
 
     def _reiterate(self):
         """This function reiterates based in the users selection"""
         pass
 
+
     def _add_phrase(self):
         pass
+
 
     def _create_migrate_file(self):
         pass
 
+
     def _output(self):
         pass
+
 
     def _gui(self):
         pass
 
-    def load(self):
-        pass
 
     def migrate(self):
         pass
 
+
     def nesting(self):
         pass
+
 
     def test_file(self, file, testColumn, answerColumn, updateReferenceFile = False):
         """This function tests know values against the AI Reference File"""
         df = pd.read_csv(file, header=0, dtype=str)
-        print(df.head())
+        #print(df.head())
         indexDictionary = {}
-        print(df.count)
+        #print(df.count)
+
 
     def trainAI(self, trainingFile, referenceFile, updateReferenceFile = False):
         """This function trains the AI"""
         trainingSet = pd.read_csv(trainingFile, header=0, dtype=str)
-        print(trainingSet.head())
+        #print(trainingSet.head())
         with open(referenceFile, 'r') as fp:
             indexDictionary = json.load(fp)
         print(indexDictionary["Belconnen Town Centre"])
 
-    def create_base_set_by_deliminator(self):
+    ##################################################################################################################################
+    # NEED TO ADD PHRASE BREAKDOWN Two, Three, Four
+    ##################################################################################################################################
+    def create_base_set_by_deliminator(self, file, newFileName, deliminator=" ", removeChars = [], fixed=True, printLastItem=False):
         """This function is used to break text into phrases by deliminator"""
-        pass
+        fileTodeliminat = open(file + ".txt", 'r')
+        deliminate = fileTodeliminat.read()
+        fileTodeliminat.close()
+        deliminate = deliminate.split(deliminator)
+        deliminatedJSON = {}
+        for item in deliminate:
+            for remove in removeChars:
+                item = item.replace(remove, "")
+            #print(item)
+            try:
+                deliminatedJSON[item] = self._deliminatBreakdown(item, fixed)
+            except:
+                pass #Duplicate value
 
+        try:    #Create JSON file
+            with open(newFileName + ".json", 'w') as fp:
+                json.dump(deliminatedJSON, fp)
+        except:
+            print("Error creating loading file")
+
+        if printLastItem == True:
+            try:
+                pp = pprint.PrettyPrinter(indent=2, width=80)
+                pp.pprint(deliminatedJSON[item])
+            except:
+                print("Invalid last item")
+
+
+
+    ##################################################################################################################################
+    # NEED TO REDUCE FUNCTION SIZE INTO SMALLER FUNCTIONS TO BE REUSED
+    ##################################################################################################################################
     def base_set(self, file, jsonFileName, indexColumn, valueColumns):
         """This function creates the json dictionary reference file to be used in the AI
 
@@ -123,85 +280,102 @@ class AI_Phraser:
             :param valueColumns:
 
         """
-        df = pd.read_csv(file, header=0, dtype=str)
-        index = df[indexColumn].tolist()
+        try:
+            df = pd.read_csv(file, header=0, dtype=str)
+        except:
+            print("Error loading CSV file")
 
-        z = 0
-        indexDictionary = {}
-        for i in index:
-            indexValues = []
+        try:
+            index = df[indexColumn].tolist()
+            z = 0
+            indexDictionary = {}
+        except:
+            print("Error selecting column")
 
-            for key in valueColumns:
-                lst = df[key].tolist()
-                indexValues.append(lst[z])
+        try:
+            for i in index:
+                indexValues = []
 
-            indexDictionary[i] = {
-                "type": "fixed",
-                "passed":[],
-                "two": {
-                    "first":[],
-                    "mid":[],
-                    "end":[]
-                },
-                "three": {
-                    "first":[],
-                    "mid":[],
-                    "end":[]
-                },
-                "four": {
-                    "first":[],
-                    "mid":[],
-                    "end":[]
-                },
-                "scores":{
-                    "allScores": [],
-                    "statistics": {
-                        "min": 0,
-                        "max": 0,
-                        "median": 0,
-                        "average": 0,
-                        "stDev": 0,
-                        "sum": 0
+                for key in valueColumns:
+                    lst = df[key].tolist()
+                    indexValues.append(lst[z])
+
+                indexDictionary[i] = {
+                    "type": "fixed",
+                    "passed":[],
+                    "two": {
+                        "first":[],
+                        "mid":[],
+                        "end":[]
                     },
-                },
-                "values": indexValues
-            }
-            z = z + 1
+                    "three": {
+                        "first":[],
+                        "mid":[],
+                        "end":[]
+                    },
+                    "four": {
+                        "first":[],
+                        "mid":[],
+                        "end":[]
+                    },
+                    "scores":{
+                        "allScores": [],
+                        "statistics": {
+                            "min": 0,
+                            "max": 0,
+                            "median": 0,
+                            "average": 0,
+                            "stDev": 0,
+                            "sum": 0
+                        },
+                    },
+                    "values": indexValues
+                }
+                z = z + 1
+        except:
+            print("Error creating JSON format")
 
-        for keys, values in indexDictionary.items():
-            strLen = len(keys)
+        try:
+            for keys, values in indexDictionary.items():
+                strLen = len(keys)
+                keys = str(keys)
 
-            for x in range(strLen-1):       #two letters
-                strShort = keys[x:x+2].lower()
-
-                if x < 1:
-                    values["two"]["first"].append([strShort,0])
-                elif x >= strLen - 2:
-                    values["two"]["end"].append([strShort,0])
-                else:
-                    values["two"]["mid"].append([strShort,0])
-
-            for x in range(strLen-2):       #three letters
-                strShort = keys[x:x+3].lower()
-
-                if x < 1:
-                    values["three"]["first"].append([strShort,0])
-                elif x >= strLen - 3:
-                    values["three"]["end"].append([strShort,0])
-                else:
-                    values["three"]["mid"].append([strShort,0])
-
-            if strLen > 8:
-
-                for x in range(strLen-3):   #four letters
-                    strShort = keys[x:x+4].lower()
+                for x in range(strLen-1):       #two letters
+                    strShort = keys[x:x+2].lower()
 
                     if x < 1:
-                        values["four"]["first"].append([strShort,0])
-                    elif x >= strLen - 4:
-                        values["four"]["end"].append([strShort,0])
+                        values["two"]["first"].append([strShort,0])
+                    elif x >= strLen - 2:
+                        values["two"]["end"].append([strShort,0])
                     else:
-                        values["four"]["mid"].append([strShort,0])
+                        values["two"]["mid"].append([strShort,0])
 
-        with open(jsonFileName + ".json", 'w') as fp:
-            json.dump(indexDictionary, fp)
+                for x in range(strLen-2):       #three letters
+                    strShort = keys[x:x+3].lower()
+
+                    if x < 1:
+                        values["three"]["first"].append([strShort,0])
+                    elif x >= strLen - 3:
+                        values["three"]["end"].append([strShort,0])
+                    else:
+                        values["three"]["mid"].append([strShort,0])
+
+                if strLen > 8:
+
+                    for x in range(strLen-3):   #four letters
+                        strShort = keys[x:x+4].lower()
+
+                        if x < 1:
+                            values["four"]["first"].append([strShort,0])
+                        elif x >= strLen - 4:
+                            values["four"]["end"].append([strShort,0])
+                        else:
+                            values["four"]["mid"].append([strShort,0])
+        except:
+            print("Error subdividing phrase")
+
+        try:    #Create JSON file
+            with open(jsonFileName + ".json", 'w') as fp:
+                json.dump(indexDictionary, fp)
+        except:
+                print("Error creating loading file")
